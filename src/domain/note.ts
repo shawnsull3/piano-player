e562c128c6cac18b1b1239640c84646c338bccd6
Shawn Sullivan <sullivansm3@gmail.com>
2020-10-04 17:44:31 -0600

    Outline keyboard range, map for numeric / literal pitches for notes

--- src/domain/note.ts
+++ src/domain/note.ts
@@ -1,3 +1,11 @@
+const C1_MIDI_NUMBER = 24
+const C4_MIDI_NUMBER = 60
+const B5_MIDI_NUMBER = 83
+
+export const LOWER_NOTE = C4_MIDI_NUMBER
+export const HIGHER_NOTE = B5_MIDI_NUMBER
+export const SEMITONES_IN_OCTAVE = 12
+
 export type NoteType = "natural" | "flat" | "sharp"
 export type NotePitch = "A" | "B" | "C" | "D" | "E" | "F" | "G"
 export type OctaveIndex = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
@@ -14,3 +22,27 @@ export interface Note {
     octave: OctaveIndex // an octave index of a given note
 }
 
+export const NATURAL_PITCH_INDICES: PitchIndex[] = [
+    0,
+    2,
+    4,
+    5,
+    7,
+    9,
+    11
+]
+  
+export const PITCHES_REGISTRY: Record<PitchIndex, NotePitch> = {
+    0: "C",
+    1: "C",
+    2: "D",
+    3: "D",
+    4: "E",
+    5: "F",
+    6: "F",
+    7: "G",
+    8: "G",
+    9: "A",
+    10: "A",
+    11: "B"
+}
