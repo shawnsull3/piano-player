242e4f030d9e6710dd581e3648c86c632627067d
Shawn Sullivan <sullivansm3@gmail.com>
2020-10-04 18:36:42 -0600

    Check if browser supports AudioContext

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+// AudioContexType = type of window.AudioContext
+type AudioContextType = typeof AudioContext
+
+interface Window extends Window {
+  webkitAudioContext: AudioContextType
+}
