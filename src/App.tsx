3b406b355965f1dc7a73b73d5d42411419cb2c59
Shawn Sullivan <sullivansm3@gmail.com>
2020-10-04 20:22:50 -0600

    Import and render main comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import { Footer } from "./components/Footer"
 import { Logo } from "./components/Logo"
+import { Main } from "./components/Main"
 import "./App.css"
 
 export const App = () => {
   return (
     <div className="app">
       <Logo />
-      <main className="app-content" />
+      <main className="app-content">
+        <Main />
+      </main>
       <Footer />
     </div>
   )
