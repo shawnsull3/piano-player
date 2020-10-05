bb369aa890480d93a1e5af1f662146ff3a13f66c
Shawn Sullivan <sullivansm3@gmail.com>
2020-10-05 11:28:01 -0600

    Create load, resume, play, stop async functions

--- src/adapters/Soundfont/useSoundfont.ts
+++ src/adapters/Soundfont/useSoundfont.ts
@@ -6,6 +6,7 @@ import {
   AudioNodesRegistry,
   DEFAULT_INSTRUMENT
 } from "../../domain/sound"
+import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from "constants"
 
 // input
 interface Settings {
@@ -24,10 +25,55 @@ interface Adapted {
 
 export function useSoundfont({ AudioContext }: Settings): Adapted {
     let activeNodes: AudioNodesRegistry = {}
-    const [current, setCurrent] = useState<Optional<InstrumentName>>(
+    const [current, setCurrent] = useState<Optional<InstrumentName>>( // current instrument loaded
       null
     )
     const [loading, setLoading] = useState<boolean>(false)
-    const [player, setPlayer] = useState<Optional<Player>>(null)
-    const audio = useRef(new AudioContext())
+    const [player, setPlayer] = useState<Optional<Player>>(null) // soundfont player instance
+    const audio = useRef(new AudioContext()) // audio context instance
+
+    async function load (
+        instrument: InstrumentName = DEFAULT_INSTRUMENT
+    ) {
+        setLoading(true)
+        const player = await Soundfont.instrument(
+            audio.current,
+            instrument
+        )
+
+        setLoading(false)
+        setCurrent(instrument)
+        setPlayer(player)
+    }
+
+    async function resume() {
+        return audio.current.state === "suspended"
+            ? await audio.current.resume()
+            : Promise.resolve()
+    }
+
+    async function play(note: MidiValue) {
+        await resume()
+        if(!player) return
+
+        const node = player.play(note.toString())
+        activeNodes = {...activeNodes, [note]: node}
+    }
+
+    async function stop(note: MidiValue) {
+        await resume()
+        if (!activeNodes[note]) return
+
+        activeNodes[note]!.stop() // Assert that activeNodes[note] is non-null and stop note
+        activeNodes = {...activeNodes, [note]: null}
+    }
+
+    return {
+        loading,
+        current,
+    
+        load,
+        play,
+        stop
+    }
 }
